d695a7deed03c3b3146b992454bcfb17b1ef5993
Johannes <jtuchscherer@gmail.com>
2011-01-17 22:48:56 -0700

    Using the DAOBase for the dao class allows to get rid of some code

--- src/main/java/org/myboulderlog/server/dao/objectify/MessageDAOImpl.java
+++ src/main/java/org/myboulderlog/server/dao/objectify/MessageDAOImpl.java
@@ -3,6 +3,7 @@ package org.myboulderlog.server.dao.objectify;
 import com.googlecode.objectify.Key;
 import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.helper.DAOBase;
 import org.myboulderlog.server.dao.MessageDAO;
 import org.myboulderlog.server.model.Message;
 
@@ -11,36 +12,26 @@ import java.util.Collection;
 
 /**
  */
-public class MessageDAOImpl implements MessageDAO {
+public class MessageDAOImpl extends DAOBase implements MessageDAO {
     /**
      * @return Collection of Message
      */
     public Collection<Message> getAll() {
-        final Objectify service = getService();
-
-        return (service.query(Message.class).list());
+        return (ofy().query(Message.class).list());
     }
 
     /**
      * @param message
      */
     public Message create(final Message message) {
-        final Objectify service = getService();
-
-        Key<Message> key = service.put(message);
-        return service.get(key);
+        Key<Message> key = ofy().put(message);
+        return ofy().get(key);
     }
 
     /**
      * @param id
      */
     public void deleteById(final Long id) {
-        final Objectify service = getService();
-
-        service.delete(Message.class, id.longValue());
-    }
-
-    private Objectify getService() {
-        return (ObjectifyService.begin());
+        ofy().delete(Message.class, id.longValue());
     }
 }
